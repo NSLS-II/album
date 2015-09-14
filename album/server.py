e1e013c0242932dc8ce536a085b87a71fd2de2f6
Eric Dill <edill@bnl.gov>
2015-09-14 11:11:15 -0400

    ENH: Plot data from mds.examples.temperature_ramp
    Plot using bokeh with bokeh.charts.TimeSeries

--- album/server.py
+++ album/server.py
@@ -9,6 +9,12 @@ from bokeh.resources import INLINE
 from bokeh.templates import RESOURCES
 from bokeh.util.string import encode_utf8
 from bokeh.charts import TimeSeries
+from bokeh.io import vplot, hplot
+import numpy as np
+import time as ttime
+import pandas as pd
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -47,11 +53,29 @@ def run_show(*args, **kwargs):
             fields.append(field)
     
     table = get_table(h, fill=False)
-    print("Table\n-----\n%s" % table)
-    plot = TimeSeries(table, legend=True)
-    # for k, v in table.items():
-    #     plot.circle(v.index, v)
-        
+    # now = ttime.time()
+    # data = {'time': [datetime.fromtimestamp(now + _) for _ in range(100)],
+    #         'd1': np.sin(np.arange(0, 1, .01)),
+    #         'd2': np.cos(np.arange(0, 1, .01))}
+    # table = pd.DataFrame(data, index=range(100))
+    plots = []
+    for k, v in table.items():
+        if k == 'time':
+            continue
+        df = pd.DataFrame(v.values, index=table['time'], columns=[k])
+        df = df.dropna()
+        plots.append(TimeSeries(df, legend=True, title=k, xlabel='time'))
+    
+    ncols = int(np.ceil(np.sqrt(len(plots))))
+    nrows = int(np.ceil(len(plots) / ncols))
+    rows = []
+    for row in range(nrows):
+        rows.append(hplot(*plots[row*ncols:(row+1)*ncols]))
+    if len(rows) == 1:
+        plot, = rows
+    else:
+        plot = vplot(*rows)
+    print('plot = %s' % plot)
     script, div = components(plot)
     
     plot_resources = RESOURCES.render(
@@ -65,58 +89,6 @@ def run_show(*args, **kwargs):
                            plot_resources=plot_resources,
                            plot_script=script,
                            bokeh_version=bokeh.__version__)
-
-colors = {
-    'Black': '#000000',
-    'Red':   '#FF0000',
-    'Green': '#00FF00',
-    'Blue':  '#0000FF',
-}
-
-
-def getitem(obj, item, default):
-    if item not in obj:
-        return default
-    else:
-        return obj[item]
-        
-        
-@app.route('/run/<uid>/plotter')
-def some_plotter(uid):
-    """ Very simple embedding of a polynomial chart"""
-    # Grab the inputs arguments from the URL
-    # This is automated by the button
-    args = flask.request.args
-
-    # Get all the form arguments in the url with defaults
-    color = colors[getitem(args, 'color', 'Black')]
-    _from = int(getitem(args, '_from', 0))
-    to = int(getitem(args, 'to', 10))
-
-    # Create a polynomial line graph
-    x = list(range(_from, to + 1))
-    fig = figure(title="Polynomial")
-    fig.line(x, [i ** 2 for i in x], color=color, line_width=2)
-
-    # Configure resources to include BokehJS inline in the document.
-    # For more details see:
-    #   http://bokeh.pydata.org/en/latest/docs/reference/resources_embedding.html#module-bokeh.resources
-    plot_resources = RESOURCES.render(
-        js_raw=INLINE.js_raw,
-        css_raw=INLINE.css_raw,
-        js_files=INLINE.js_files,
-        css_files=INLINE.css_files,
-    )
-
-    # For more details see:
-    #   http://bokeh.pydata.org/en/latest/docs/user_guide/embedding.html#components
-    script, div = components(fig, INLINE)
-    html = flask.render_template(
-        'embed_plot.html',
-        plot_script=script, plot_div=div, plot_resources=plot_resources,
-        color=color, _from=_from, to=to
-    )
-    return encode_utf8(html)
 
 if __name__ == '__main__':
     app.run(debug=True)
